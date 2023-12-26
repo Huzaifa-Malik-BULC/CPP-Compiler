6d78d56f52e3683732cacd023cc1d45d3503d203
Huzaifa-Malik-BULC <malik74pk@gmail.com>
2023-12-26 18:31:51 +0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+#include "Token Generation.cpp"
+using namespace std;
+
+int main() {
+	char choice;
+	cout << "1. Generate Tokens" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	cout << "1. Tokenize" << endl;
+	if (choice == '1') {
+		tokenize();
+	}
+}
